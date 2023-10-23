f22e4134df3003ac61e37c08aeef0b13a96d3a6a
Ericriera <ericrp2002@gmail.com>
2023-10-23 18:06:33 +0200

    add: google login

--- app/index.jsx
+++ app/index.jsx
@@ -1,24 +1,86 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'expo-router';
 import { Image, View, Text, Button, StyleSheet } from 'react-native';
 import colors from '../constants/colors';
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Page() {
+    const [userInfo, setUserInfo] = React.useState(null);
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        webClientId: 'CLIENT_ID',
+        iosClientId: '852693017999-3bur3t29c1stjg1ft95njoagkjfao394.apps.googleusercontent.com',
+        androidClientId: '852693017999-rmrneotl5b6j5p5sf5ukequup60rtrmi.apps.googleusercontent.com',
+
+    });
+
+    useEffect(() => {
+        handleSignInWithGoogle();
+    }, [response])
+
+    async function handleSignInWithGoogle() {
+        const user = await getLocalUser();
+        if (!user) {
+            if (response?.type === "success") {
+                getUserInfo(response.authentication.accessToken);
+            }
+        }
+        else {
+            setUserInfo(user);
+        }
+    }
+
+    const getLocalUser = async () => {
+        const data = await AsyncStorage.getItem("@user");
+        if (!data) return null;
+        return JSON.parse(data);
+    }
+
+    const getUserInfo = async (token) => {
+        if (!token) return;
+        try {
+            const response = await fetch(
+                "https://www.googleapis.com/userinfo/v2/me",
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            );
+            const user = await response.json();
+            await AsyncStorage.setItem("@user", JSON.stringify(user));
+            setUserInfo(user);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+
     return (
         <View style={styles.container}>
-            <Image
-                style={{ margin: 15, width: 270, height: 75 }}
-                source={require('../assets/full-logo.png')}
-            />
-            <View style={styles.centeredContent}>
-                <Text style={styles.title}>Benvingut</Text>
-                <Text style={styles.subtitle}>Sisplau inicia sesió amb google per poder accedir</Text>
-            </View>
-            <View style={styles.centeredContent2}>
-                <Link href={'/(tabs)/home'} replace asChild>
-                    <Button title='Log in' />
-                </Link>
-            </View>
+            {!userInfo ? (
+                <View>
+                    <Image
+                        style={{ margin: 15, width: 270, height: 75 }}
+                        source={require('../assets/full-logo.png')}
+                    />
+                    <View style={styles.centeredContent}>
+                        <Text style={styles.title}>Benvingut</Text>
+                        <Text style={styles.subtitle}>Sisplau inicia sesió amb google per poder accedir</Text>
+                    </View>
+                    <View style={styles.centeredContent2}>
+                        <Link href={'/(tabs)/home'} replace asChild>
+                            <Button
+                                title='Log in'
+                                disabled={!request}
+                                onPress={() => {
+                                    promptAsync();
+                                }}
+                            />
+                        </Link>
+                    </View>
+                </View>) : (<View></View>)}
         </View>
     );
 }
@@ -27,16 +89,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffffff',
-        alignItems: 'center', // Centrar horizontalmente
-        justifyContent: 'flex-start', // Colocar la imagen en la parte superior
+        alignItems: 'center',
+        justifyContent: 'flex-start',
     },
     centeredContent: {
-        flex: 1, // Para que los elementos ocupen todo el espacio vertical
-        justifyContent: 'center', // Centrar verticalmente en el espacio disponible
+        flex: 1,
+        justifyContent: 'center',
     },
     centeredContent2: {
-        alignItems: 'center', // Centrar horizontalmente
-        flex: 1, // Para que los elementos ocupen todo el espacio vertical
+        alignItems: 'center',
+        flex: 1,
     },
     title: {
         fontSize: 30,
