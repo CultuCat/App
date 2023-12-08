b05e24476b3934014ee959383f3ad1e45e41e2eb
{nuriacanals3} <nuria.canals4@gmail.com>
2023-12-08 14:47:01 +0100

    home i user translations

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,13 +1,99 @@
-import React from 'react';
-import { Link } from 'expo-router';
-import { Text, Button, View, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, FlatList, StyleSheet, SafeAreaView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import TicketCard from '../components/ticketCard.jsx';
+import { useNavigation } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
+
 
 export default function Page() {
-    return (
-        <View>
-            <Text>Home page</Text>
-            
-            
-        </View>
-    );
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState('');
+  const {t} =useTranslation();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userInfo = await getLocalUser();
+      fetchAllEvents(userInfo.map(item => item.id))
+        .then(allEvents => {
+          setEvents(allEvents.flat());
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error('Error fetching events:', error);
+        });
+    };
+    fetchData();
+  }, []);
+
+  const fetchEventsById = async (id) => {
+    const response = await fetch(`https://cultucat.hemanuelpc.es/events/?espai=${id}`);
+    const data = await response.json();
+    return data.results;
+  }
+
+  const fetchAllEvents = async (ids) => {
+    const promises = ids.map(id => fetchEventsById(id));
+    const allEvents = await Promise.all(promises);
+    return allEvents;
+  }
+
+  const getLocalUser = async () => {
+    try {
+      const dataString = await AsyncStorage.getItem("@user");
+      if (!dataString) return null;
+      const data = JSON.parse(dataString);
+      setName(data.user.first_name);
+      return data.user.espais_preferits;
+    } catch (error) {
+      console.error('Error getting local user data:', error);
+      return null;
+    }
+  };
+
+  const navigation = useNavigation();
+  const handlePressEvent = (eventId) => {
+    navigation.navigate('event', { eventId });
+  };
+
+  const renderItem = ({ item }) => (
+    <TicketCard
+      event={item.nom}
+      data={item.dataIni}
+      imatge={item.imatges_list[0]}
+      espai={item.espai.nom}
+      onPress={() => handlePressEvent(item.id)}
+    />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+        {loading ? (
+          <Text>{t('Carregant')}</Text>
+        ) : (
+          <View style={{flex: 1}}>
+            <Text style={styles.title}>{t('Index.Benvingut')}, {name}</Text>
+            <FlatList
+              data={events}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id.toString()}
+              contentContainerStyle={{ alignItems: 'center' }}
+            />
+          </View>
+        )}
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 25,
+    marginVertical: 10,
+    marginHorizontal: '5%',
+  },
+});
