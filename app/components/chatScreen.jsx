42ea04a992f6c8ea75fd6adf73431905af10349a
{nuriacanals3} <nuria.canals4@gmail.com>
2023-12-17 14:53:09 +0100

    send message i historial

--- app/components/chatScreen.jsx
+++ app/components/chatScreen.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { View, Text,StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
+import { View, Text,StyleSheet, Image, TouchableOpacity, Modal, TextInput, Keyboard, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 
@@ -10,48 +10,106 @@ const ChatScreen = ({ user, userLId, onBack}) => {
   const uIdR = user.id; //Remote user id
   const [url, setUrl] = useState('https://cultucat.hemanuelpc.es');
 
-  useEffect(() => {
-    const fetchMessages = async () => {
-      try {
-        const response = await fetch(`${url}/messages/?user1=${uId}&user2=${uIdR}`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setMessages(data);
-      } catch (error) {
-        console.error('Error fetching messages:', error);
-      }
-    };
 
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch(`${url}/messages/?user1=${uId}&user2=${uIdR}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setMessages(data);
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+    }
+  };
+
+  useEffect(() => {
     fetchMessages();
   }, [uId, uIdR, url]);
+
+  const sendMessage = async () => {
+    try {
+      const response = await fetch(`${url}/messages/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_from: uId,
+          user_to: uIdR,
+          text: newMessage,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      setNewMessage('');
+      fetchMessages(uId,uIdR,url);
+
+    } catch (error) {
+      console.error('Error submitting message:', error);
+    }
+  };;
+
+  const renderMessageItem = ({ item }) => {
+    const isSentByUser = item.user_from === uId;
+
+    return (
+      <View style={[styles.messageContainer, isSentByUser ? styles.sentMessage : styles.receivedMessage]}>
+        <Text style={styles.messageText}>{item.text}</Text>
+      </View>
+    );
+  };
+
+  const handleScreenTap = () => {
+    Keyboard.dismiss(); 
+  };
   
   return (
     <Modal
       animationType="none"
       onRequestClose={onBack}
     >
-      <View style={styles.container}>
-          <View style={styles.userInfo}>
-              <TouchableOpacity onPress={onBack} style={styles.backButton}>
-                  <Ionicons name="chevron-back" size={20} color="white" />
-              </TouchableOpacity>
-              <View style={styles.userDetails}>
-                  <Image source={{ uri: user.imatge }} style={styles.userImage} />
-                  <Text style={styles.userName}>{user.first_name} </Text>
-              </View>
+      <View style={styles.userInfo}>
+        <TouchableOpacity onPress={onBack} style={styles.backButton}>
+            <Ionicons name="chevron-back" size={20} color="white" />
+        </TouchableOpacity>
+        <View style={styles.userDetails}>
+            <Image source={{ uri: user.imatge }} style={styles.userImage} />
+            <Text style={styles.userName}>{user.first_name} </Text>
         </View>
       </View>
-       
+      <View style={styles.messagesBox}>
+        <FlatList
+          data={messages}
+          renderItem={renderMessageItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+        <TouchableOpacity
+          onPress={handleScreenTap}
+        >
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.chatbox}>
+        <TextInput
+            multiline
+            placeholder={"Type something..."}
+            style={styles.input}
+            value={newMessage}
+            onChangeText={(text) => setNewMessage(text)}
+        />
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
+          <Ionicons name="send" size={20} color='white'/>
+        </TouchableOpacity>
+      </View>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
     userInfo: {
       flexDirection: 'row',
       backgroundColor: '#ff6961',
@@ -62,7 +120,6 @@ const styles = StyleSheet.create({
     userDetails: {
       flexDirection: 'row',
       alignItems: 'center',
-      flex: 1,
     },
     userImage: {
       width: 60,
@@ -77,6 +134,57 @@ const styles = StyleSheet.create({
     },
     backButton: {
       marginRight: 20,
+    },
+    chatbox: {
+      paddingHorizontal: 10,
+		  justifyContent: "space-between",
+		  alignItems: "center",
+		  flexDirection: "row",
+		  paddingVertical: 10,
+      paddingBottom: 30,
+      backgroundColor: "#ff6961",
+    },
+    input: {
+      backgroundColor: "white",
+      paddingLeft: 20,
+      marginRight: 20,
+      marginLeft: 20,
+      color: "grey",
+      flex: 3,
+      fontSize: 15,
+      height: 30,
+      alignSelf: "center",
+      borderRadius: 20,
+    },
+    sendButton: {
+      backgroundColor: "blue",
+      borderRadius: 50,
+      height: 40,
+      width: 40,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    messagesBox: {
+      flex: 1,
+      padding: 10,
+      paddingTop: 20,
+    },
+    messageContainer: {
+      maxWidth: '80%',
+      borderRadius: 8,
+      padding: 8,
+      marginBottom: 8,
+    },
+    sentMessage: {
+      alignSelf: 'flex-end',
+      backgroundColor: '#87ceec',
+    },
+    receivedMessage: {
+      alignSelf: 'flex-start',
+      backgroundColor: '#DCF8C5',
+    },
+    messageText: {
+      fontSize: 16,
     },
 });
 
