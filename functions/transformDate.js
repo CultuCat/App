cd2c55f67a6d00dec02eaa06ed297243a7599b20
Eric Riera <ericrp2002@gmail.com>
2023-12-30 19:08:49 +0100

    fix: formatter

--- functions/transformDate.js
+++ functions/transformDate.js
@@ -1,11 +1,11 @@
 export const transformDate = (date) => {
-    const dateObj = new Date(date);
-    const formatOptions = {
-      weekday: 'short',
-      month: 'short',
-      day: 'numeric',
-      year: 'numeric',
-    };
-    const formatter = new Intl.DateTimeFormat('en-US', formatOptions);
-    return formatter.format(dateObj);
+  const dateObj = new Date(date);
+  const formatOptions = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
   };
+  const formatter = new Intl.DateTimeFormat('en-US', formatOptions);
+  return formatter.format(dateObj);
+};
